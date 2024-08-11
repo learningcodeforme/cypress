f3f25ca839bc281e8f94deb46ac3a508be7a4b12
Nitesh Kumar <nitesh.hope@gmail.com>
2024-08-11 18:25:24 +0530

    cypress code changes

--- cypress/Integration/examples/test.spec.js
+++ cypress/Integration/examples/test.spec.js
@@ -1,8 +1,76 @@
-
+/// <reference types= "cypress" />  
 
 describe("Test Suite", function () {
     it("Test case", function () {
-        cy.visit('https://docs.cypress.io/guides/references/changelog#13-13-2')
+        // any command start in cypress with cy 
+        //visit() -to launch 
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/')
+        //get() to grabe the locator
+        //type() to type text
+        cy.get('form input').type('ca')
+        //wait() to make the test to wait
+        cy.wait(2000)
+        // parent child chain  
+        // .should() assertion 
+        //'have.lenght -- to validate lenght give as second arrgument
+        cy.get('.products .product').should('have.length', 4)
+        //.find() -- > to chain the locator with parent laocator and reduce 
+        //scope of search within the parent locator 
+        //eq() ->  to get index of array
+        //contains()- to search for texr
+        cy.get('.products').as('productLocator')
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART')
+            .click()
+        cy.wait(2000)
+        //each() -Iterate through an array like structure (arrays or objects with a length property).
+        // #el is used to iterate to list
+        cy.get('.products .product').each(($el, index, $list) => {
+            // .text() -to grab the text 
+            const textVeg = $el.find('h4.product-name').text()
+            //cy.log() - to print anything on console
+            cy.log(textVeg)
+            //include() it js method to validate string
+            if (textVeg.includes('Cashews')) {
 
+                //cy.wrap to sync the js other wise it not wait for promise ()
+                cy.wrap($el).find('button').click()
+            }
+        })
+        //text() -it is jquery command 
+        // cypress can allow jquery but it cannot handle promise ( we need to manually solve it)
+
+        //assersation -- should with have-text
+        cy.get('.brand').should('have.text', 'GREENKART')
+
+        const logo = cy.get('.brand').then((logo) => {
+            cy.log(logo.text())
+
+            // alias - as()  we dfine as locator aslia and call the using 
+            //@alias- name ( check in line number 49)
+            cy.get('.products').as('productLocator')
+            cy.get('@productLocator').find('.product').eq(0).contains('ADD TO CART')
+                .click()
+
+            // console.log is not cypess command so it async and it can execute any time .it appear in console of browser ( press F12 and check in console tab of browser)
+            console.log('Printing in console')
+        })
+        //  cy.log(logo.text())
+
+    })
+    it('Checkout the cart', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/')
+        cy.get('form input').type('ca')
+        cy.wait(2000)
+        cy.get('.products .product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            cy.log(textVeg)
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click()
+            }
+        })
+
+        cy.get('.cart-icon').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+        cy.contains('Place Order').click()
     })
 })
--- cypress/Integration/examples/uiContols.js
+++ cypress/Integration/examples/uiContols.js
@@ -81,7 +81,7 @@ describe("UI controls", () => {
 
     })
 
-    it.only('Handling child widow', () => {
+    it('Handling child widow', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
         // we have to remove target from new table it set as _blank id DOM 
         //  it mean it open the url in new tab
@@ -94,6 +94,51 @@ describe("UI controls", () => {
             cy.get(".mt-50 h2").should('contain', 'QAClick Academy');
 
         })
+
+    })
+
+    it('Handling table in cyress ', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        // we have table in which we found that particular text("Master Selenium Automation in simple Python Language") and from that we have to get the detail of that row and find the  price of particular text  that is present in next column
+
+        cy.get('.table-display tr td:nth-child(2)').each(($el, index, $list) => {
+            const text = $el.text()
+
+            if (text === 'Master Selenium Automation in simple Python Language') {
+                cy.log(text)
+                // in  cypress we have next() to get sibliing element 
+                //eq we pass index to particular text and then we apply next()
+                // next() only work on get command  
+                // we have to solve the promise to get text() as it is 
+                cy.get('.table-display tr td:nth-child(2)').eq(index).next()
+                    .then((price) => {
+                        const priceText = price.text()
+                        expect(priceText).to.equal('25')
+                    })
+
+            }
+
+        })
+    })
+
+
+    it.only('Mouse over in cyress ', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        //cypress dont have any method to handle mouse over 
+        // we have to use jquery show method 
+        // to call jquery show method we have to  use .invoke()
+        cy.get('.mouse-hover-content').invoke('show')
+        // click the top
+        cy.contains('Top').click()
+        // //verify the url should contains "top" as tex
+        cy.url().should('include', 'top')
+
+        //cypress have ability to click hindden element using 
+        //  .click({force:true})
+
+        // to handle invisible element
+        cy.contains('Top').click({ force: true })
+        cy.url().should('include', 'top')
 
 
     })
