a8f6f529bb61c005332a926b055edea0d6327956
Nitesh Kumar <nitesh.hope@gmail.com>
2024-11-19 06:05:45 +0530

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "7o3tsz",
+  defaultCommandTimeout: 6000,
+  reporter: 'cypress-mochawesome-reporter',
+  env: {
+    url: "https://rahulshettyacademy.com"
+  },
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+
     specPattern: 'cypress/Integration/examples/*.js',
   },
 });
